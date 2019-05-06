0a71f0ec76477f0fd9b94da83227fd915e75a96b
Maurice Mauser <maurice.mauser@gmail.com>
2019-05-06 00:45:17 +0000

    fix: update state with books

--- src/Shelves/Shelf.js
+++ src/Shelves/Shelf.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from '../Books/Book';
 
 const Shelf = ({ shelf, books = [], onSelectShelf }) => (
   <div className="bookshelf">
-  <h2 className="bookshelf-title">{shelf}</h2>
-<div className="bookshelf-books">
-<ol className="books-grid">
-{books.map(book => <Book key={book.id} book={book} onSelectShelf={onSelectShelf} />)}
-</ol>
-</div>
-</div>
+    <h2 className="bookshelf-title">{shelf}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(book => <Book key={book.id} book={book} onSelectShelf={onSelectShelf} />)}
+      </ol>
+    </div>
+  </div>
 );
 export default Shelf;
